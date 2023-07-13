24c499024b83a908ee885c2f7031a8b0cf347d2d
M Zia <murtaza_zia@hotmail.co.uk>
2023-07-13 12:50:12 +0100

    finished and tested the destructuring arrays task

--- modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
+++ modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
@@ -1,9 +1,11 @@
-// Instructions can be found in destructuring_arrays.md
+// Instructions can be found in destructuring_arrays.md/
 
 export function sequence(order) {
-  const first = order[0];
-  const second = order[1];
-  const third = order[2];
+  // const first = order[0];
+  // const second = order[1];
+  // const third = order[2];
+
+  const [first, second, third] = order;
 
   return first + ", " + second + ", " + third;
 }
