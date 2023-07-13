64df392ed80da25a75db0dfa95f76b65dce91c57
M Zia <murtaza_zia@hotmail.co.uk>
2023-07-13 18:29:26 +0100

    completed and tested rest_parameter task

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,10 +1,21 @@
 // Instructions can be found in rest_parameters.md
 
-function add(a, b) {
-  // Add a loop here
-  return a + b;
+export function add(a, b, ...otherNumbers) {
+
+  let total = a + b;
+  
+  // for (let i=0; i<otherNumbers.length; i++){
+  //   total += otherNumbers[i]
+  // }
+
+  for (let number of otherNumbers) {
+    total += number;
+  }
+
+
+  return total;
+
 }
 
 add(1, 2, 3, 4, 5);
 
-module.exports = add;
